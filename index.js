6bc606723c927d92dd2b10ec108574c6f095f5e1
Aman Kadian <53035847+aman843@users.noreply.github.com>
2023-04-19 17:50:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+console.log("constant .let.variable")
+let a=4
+let b=4
+b="area"
+console.log(b)
+
+const author ="harry"
+console.log(author)
+//constant can not be changed not be changed            
